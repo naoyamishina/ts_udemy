736107008a9b90a57801dc5ff8a38c8bf590ccd2
mishina naoya <msnyxx811@gmail.com>
2023-07-20 18:02:17 +0900

    ref

--- vue3-typescript/src/views/HomeView.vue
+++ vue3-typescript/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import RefComponent from '@/components/RefComponent.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <RefComponent />
   </main>
 </template>
