954fb5e05a4d0b0b2a3b849f3057d4774705060e
mishina naoya <msnyxx811@gmail.com>
2023-07-21 13:52:37 +0900

    fin

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UseStateComponent from './components/UseStateComponent';
+import EventComponent from './components/EventComponent';
+import PropsComponent from './components/PropsComponent';
+import FCPropsComponent from './components/FCPropsComponent';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UseStateComponent />
+      <EventComponent/>
+      <PropsComponent name="田中"/>
+      <FCPropsComponent name="FC田中"/>
     </div>
   );
 }
